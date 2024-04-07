3c70055ca6778d2aac71ce6e1e9b8041ea67322b
Akshara <amuppall06@gmail.com>
2024-04-07 15:40:28 +0530

    player2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Dice from "./component/Dice";
 function App() {
   const [dice, setDice] = useState(0);
   const [numOfDuplicateDiceThrows, setNumOfDuplicateDiceThrows] = useState(0);
+  const [playerTurn, setPlayerTurn] = useState(0);
+
   const onDiceClick = () => {
     setDice((prevValue) => {
       const newDiceValue = Math.floor(Math.random() * 6) + 1;
@@ -16,11 +18,18 @@ function App() {
       }
       return newDiceValue;
     });
+
+    setPlayerTurn((prev) => (prev === 0 ? 1 : 0));
   };
 
   return (
     <div>
-      <Board numOfDuplicateDiceThrows={numOfDuplicateDiceThrows} diceValue={dice} setDice={setDice} />
+      <Board
+        numOfDuplicateDiceThrows={numOfDuplicateDiceThrows}
+        diceValue={dice}
+        setDice={setDice}
+        playerTurn={playerTurn}
+      />
       <Dice onClick={onDiceClick} diceValue={dice} />
     </div>
   );
--- src/component/Board/index.jsx
+++ src/component/Board/index.jsx
@@ -7,13 +7,17 @@ const Board = ({
   diceValue,
   setDice,
   numOfDuplicateDiceThrows = 0,
+  playerTurn,
 }) => {
-  const [playerPosition, setPlayerPosition] = useState({ row: 0, col: 0 });
+  const [player1Position, setPlayer1Position] = useState({ row: 0, col: 0 });
+
+  const [player2Position, setPlayer2Position] = useState({ row: 0, col: 0 });
 
   const [ladderOne, setLadderOne] = useState({
     head: { row: 0, col: 1 },
     tail: { row: 2, col: 4 },
   });
+
   const [ladderTwo, setLadderTwo] = useState({
     head: { row: 4, col: 6 },
     tail: { row: 6, col: 5 },
@@ -23,6 +27,7 @@ const Board = ({
     head: { row: 3, col: 6 },
     tail: { row: 1, col: 1 },
   });
+
   const [snakeTwo, setSnakeTwo] = useState({
     head: { row: 7, col: 6 },
     tail: { row: 4, col: 4 },
@@ -31,96 +36,137 @@ const Board = ({
   useEffect(() => {
     //ladderone
     if (
-      playerPosition.row === ladderOne.head.row &&
-      playerPosition.col === ladderOne.head.col
+      player1Position.row === ladderOne.head.row &&
+      player1Position.col === ladderOne.head.col
     ) {
-      setPlayerPosition(ladderOne.tail);
+      setPlayer1Position(ladderOne.tail);
       return;
     }
     // laddertwo
 
     if (
-      playerPosition.row === ladderTwo.head.row &&
-      playerPosition.col === ladderTwo.head.col
+      player1Position.row === ladderTwo.head.row &&
+      player1Position.col === ladderTwo.head.col
     ) {
-      setPlayerPosition(ladderTwo.tail);
+      setPlayer1Position(ladderTwo.tail);
       return;
     }
 
     //snakeOne
     if (
-      playerPosition.row === snakeOne.head.row &&
-      playerPosition.col === snakeOne.head.col
+      player1Position.row === snakeOne.head.row &&
+      player1Position.col === snakeOne.head.col
     ) {
-      setPlayerPosition(snakeOne.tail);
+      setPlayer1Position(snakeOne.tail);
       return;
     }
     //snakeTwo
     if (
-      playerPosition.row === snakeTwo.head.row &&
-      playerPosition.col === snakeTwo.head.col
+      player1Position.row === snakeTwo.head.row &&
+      player1Position.col === snakeTwo.head.col
     ) {
-      setPlayerPosition(snakeTwo.tail);
+      setPlayer1Position(snakeTwo.tail);
       return;
     }
-  }, [playerPosition, ladderOne, ladderTwo, snakeOne, snakeTwo]);
+  }, [player1Position, ladderOne, ladderTwo, snakeOne, snakeTwo]);
 
-  console.log(playerPosition);
+  //player 2 useeffect
+  useEffect(() => {
+    //ladderone 
+    if (
+      player2Position.row === ladderOne.head.row &&
+      player2Position.col === ladderOne.head.col
+    ) {
+      setPlayer2Position(ladderOne.tail);
+      return;
+    }
+    // laddertwo
 
+    if (
+      player2Position.row === ladderTwo.head.row &&
+      player2Position.col === ladderTwo.head.col
+    ) {
+      setPlayer2Position(ladderTwo.tail);
+      return;
+    }
+
+    //snakeOne
+    if (
+      player2Position.row === snakeOne.head.row &&
+      player2Position.col === snakeOne.head.col
+    ) {
+      setPlayer2Position(snakeOne.tail);
+      return;
+    }
+    //snakeTwo
+    if (
+      player2Position.row === snakeTwo.head.row &&
+      player2Position.col === snakeTwo.head.col
+    ) {
+      setPlayer2Position(snakeTwo.tail);
+      return;
+    }
+  }, [player2Position, ladderOne, ladderTwo, snakeOne, snakeTwo]);
+
+
+  const updatePlayerPosition = (prevPoss) => {
+    const row = prevPoss.row;
+    const col = prevPoss.col;
+    // prevents player from going beyong the last row and col i.e tile
+    if (row === numOfRows - 1 && col === numOfCols - 1) {
+      return prevPoss;
+    }
+
+    let newCol = col;
+    let newRow = row;
+
+    for (let val = 0; val < diceValue; val++) {
+      // if player reached end of the row
+      if (newCol + 1 >= numOfCols) {
+        // move player to first col
+        newCol = 0;
+        // if player reached the last row
+        //1.resets players position to previous col position
+        //2. stops player from going to the out of the board (row is not incresed)
+        if (newRow + 1 >= numOfCols) {
+          newCol = col;
+          break;
+        }
+        //move player to next row
+        newRow = newRow + 1;
+        continue;
+      }
+      // if player is not able to move to the next row and has reached the end of the row than reset player position
+      if (newCol + 1 >= numOfCols) {
+        break;
+      }
+      newCol += 1;
+    }
+
+    return {
+      row: newRow,
+      col: newCol,
+    };
+  };
+  
   useEffect(() => {
     if (diceValue === 0) {
       return;
     }
-
-    setPlayerPosition((prevPoss) => {
-      const row = prevPoss.row;
-      const col = prevPoss.col;
-      // prevents player from going beyong the last row and col i.e tile
-      if (row === numOfRows - 1 && col === numOfCols - 1) {
-        return prevPoss;
-      }
-
-      let newCol = col;
-      let newRow = row;
-
-      for (let val = 0; val < diceValue; val++) {
-        // if player reached end of the row
-        if (newCol + 1 >= numOfCols) {
-          // move player to first col
-          newCol = 0;
-          // if player reached the last row
-          //1.resets players position to previous col position
-          //2. stops player from going to the out of the board (row is not incresed)
-          if (newRow + 1 >= numOfCols) {
-            newCol = col;
-            break;
-          }
-          //move player to next row
-          newRow = newRow + 1;
-          continue;
-        }
-        // if player is not able to move to the next row and has reached the end of the row than reset player position
-        if (newCol + 1 >= numOfCols) {
-          break;
-        }
-        newCol += 1;
-      }
-
-      return {
-        row: newRow,
-        col: newCol,
-      };
-    });
+    if (playerTurn === 0) {
+      setPlayer1Position(updatePlayerPosition);
+    } else {
+      setPlayer2Position(updatePlayerPosition);
+    }
     console.log(diceValue);
-    console.log(playerPosition);
-  }, [diceValue, numOfDuplicateDiceThrows]);
+  }, [diceValue, numOfDuplicateDiceThrows,playerTurn]);
   const board = [...Array(numOfRows).keys()].map((row) => {
     return [...Array(numOfCols).keys()].map((col) => {
       return row * numOfCols + col;
     });
   });
 
-  console.log(playerPosition);
+  console.log(player1Position);
 
   return (
     <div className="background">
@@ -144,9 +190,15 @@ const Board = ({
                     ${isFirstRow ? "board__col--border-bottom" : ""} 
                     ${isFirstCol ? "board__col--border-left" : ""} 
                     ${
-                      playerPosition.row === rowIdx &&
-                      playerPosition.col === colIdx
-                        ? "board__col--player"
+                      player1Position.row === rowIdx && //player1
+                      player1Position.col === colIdx
+                        ? "board__col--player1"
+                        : ""
+                    }
+                    ${
+                      player2Position.row === rowIdx && //player2
+                      player2Position.col === colIdx
+                        ? "board__col--player2"
                         : ""
                     }
                   
